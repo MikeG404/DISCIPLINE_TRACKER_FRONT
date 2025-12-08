bf01fa4581dfc1c98ea8d7d7ea2f1cb316975010
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-12-08 09:23:43 +0100

    Enhance IconButton component with variant support and update Dashboard layout for improved positioning

--- app/components/ui/IconButton.tsx
+++ app/components/ui/IconButton.tsx
@@ -1,21 +1,30 @@
 'use client'
 
-import { Code } from "lucide-react";
+import { Code, Plus } from "lucide-react";
+
+const variants = {
+    "primary": "bg-transparent text-white",
+    "secondary": "flex justify-center bg-orange-500 text-white rounded-2xl w-8 h-8"
+}
+
+type VariantKey = keyof typeof variants;
 
 type ButtonProps = {
     onClick: () => void;
+    variant?: VariantKey
 }
 
-export default function Button({onClick}: ButtonProps) {
+
+export default function Button({onClick, variant = "primary"}: ButtonProps) {
 
     return (
-        <div className="flex items-center">
+        <div className={`flex items-center ${variants[variant]}`}>
             <button 
                 type="button" 
                 className="cursor-pointer" 
                 onClick={onClick}
                 >
-                <Code color="white"/>
+                { variant === "primary" ? <Code/> : <Plus />}
             </button>
         </div>
     )
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import HeatmapContainer from "../components/containers/Heatmap.container";
 import Heatmap from "../components/Heatmap";
+import IconButton from "../components/ui/IconButton";
 
 export default function Dashboard() {
   return (
-    <main className="p-4">
+    <main className="h-screen relative p-4">
       <HeatmapContainer />
+      <div className="absolute right-4 bottom-4">
+        <IconButton 
+          onClick={() => undefined}
+          variant="secondary"
+          />
+      </div>
    </main>
   );
 }
